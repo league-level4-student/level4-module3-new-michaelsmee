ebbde9188454a59ea1368057620c6e59891c619d
jereycui2008 <jeremycuid@gmail.com>
2024-01-06 14:26:34 -0800

    Completed Spies

--- src/_01_Spies_On_A_Train/SpiesOnATrain.java
+++ src/_01_Spies_On_A_Train/SpiesOnATrain.java
@@ -8,66 +8,72 @@ import _00_Intro_to_Linked_Lists.Node;
 
 public class SpiesOnATrain {
 
-    /*
-     * A spy has made off with important intel from your intelligence agency!
-     * You know the spy is somewhere on this train(LinkedList). Your job is to
-     * find the suspect that matches the description given to you by the list of
-     * clues(the array).
-     * 
-     * Walk through the train, questioning each of the passengers about what
-     * they have seen and return the name of the most likely suspect.
-     * 
-     * The results are randomly generated each time so you should have a general
-     * case solution that carefully compares the clues to each passenger's
-     * testimony. Remember to use String methods to break up the passengers'
-     * statements.
-     */
-    String findIntel(LinkedList<TrainCar> train, String[] clues) {
-    	
-    	for(int i = 0; i<clues.length; i++) {
-    		System.out.println(clues[i]);
-    		
+	/*
+	 * A spy has made off with important intel from your intelligence agency! You
+	 * know the spy is somewhere on this train(LinkedList). Your job is to find the
+	 * suspect that matches the description given to you by the list of clues(the
+	 * array).
+	 * 
+	 * Walk through the train, questioning each of the passengers about what they
+	 * have seen and return the name of the most likely suspect.
+	 * 
+	 * The results are randomly generated each time so you should have a general
+	 * case solution that carefully compares the clues to each passenger's
+	 * testimony. Remember to use String methods to break up the passengers'
+	 * statements.
+	 */
+	String findIntel(LinkedList<TrainCar> train, String[] clues) {
 
-    	}
-    	
-    	
-    		ArrayList<String> leads = new ArrayList<String>();
-    		Node<TrainCar> current= train.getHead();
-    		for(int i = 0; i<train.size()-1; i++) {
-    			
-    			String testimony = current.getValue().questionPassenger();
-    			System.out.println(current.getValue().questionPassenger());
-    			
-    			int startName = testimony.indexOf("I saw") + 6;
-    			
-    			String evidence = testimony.substring(startName);
-    			String[] list = evidence.split(evidence);
-    			String suspectName = list[0];
-    			
-    			
-    			
-    			
-    			
-    		for(int x = 0; x<clues.length; x++) {	
-    			
-    			
-    			
-    		if(current.getValue().questionPassenger().contains(clues[x])){
-    			leads.add(current.getValue().toString());
-    		}
-    			
-    		}
-    		
-    		current = current.getNext();
-    		
-    		
-    	
-    	}
-    	
-    	
-    	
-        return "";
+		for (int i = 0; i < clues.length; i++) {
+			System.out.println(clues[i]);
 
-    }
+		}
+
+		HashMap<String, Integer> cluesList = new HashMap<String, Integer>();
+
+		ArrayList<String> leads = new ArrayList<String>();
+		Node<TrainCar> current = train.getHead();
+		for (int i = 0; i < train.size() - 1; i++) {
+
+			String testimony = current.getValue().questionPassenger();
+			System.out.println(current.getValue().questionPassenger());
+
+			int startName = testimony.indexOf("I saw") + 6;
+
+			String evidence = testimony.substring(startName);
+			String[] list = evidence.split(" ");
+			// String suspectName = list[0];
+			String suspectName = list[0];
+
+			for (int x = 0; x < clues.length; x++) {
+
+				// String Q = current.getValue().questionPassenger().toString();
+				if (current.getValue().questionPassenger().contains(clues[x])) {
+					leads.add(current.getValue().toString());
+					//String spy = evidence.substring(startName, evidence.indexOf(' '));
+					if(cluesList.containsKey(suspectName)) {
+					cluesList.put(suspectName, cluesList.get(suspectName) + 1);
+					}
+					else {
+					cluesList.put(suspectName, 1);
+					}
+				}
+
+			}
+
+			current = current.getNext();
+
+		}
+		
+		for(String key: cluesList.keySet()) {
+			if(cluesList.get(key) == 3) {
+				return key;
+			}
+		}
+		
+		
+		return "";
+
+	}
 
 }
